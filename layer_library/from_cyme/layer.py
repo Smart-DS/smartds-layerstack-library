f6b19d2bb8bbbb015bf0055b35118587003113df
ngensoll <nicolas.gensollen@nrel.gov>
2018-05-15 16:52:46 -0600

    Changes to the from_cyme layer

--- layer_library/from_cyme/layer.py
+++ layer_library/from_cyme/layer.py
@@ -31,17 +31,51 @@ class FromCyme(LayerBase):
     @classmethod
     def kwargs(cls):
         kwarg_dict = super().kwargs()
-        kwarg_dict['base_dir'] = Kwarg(default=None, description='Base directory for argument paths.')
+        kwarg_dict['base_dir'] = Kwarg(default=None, description='Base directory for argument paths.',
+                                         parser=None, choices=None,
+                                         nargs=None, action=None)
+        kwarg_dict['network_filename'] = Kwarg(default=None, description='Name of the network file',
+                                         parser=None, choices=None,
+                                         nargs=None, action=None)
+        kwarg_dict['equipment_filename'] = Kwarg(default=None, description='Name of the equipment file',
+                                         parser=None, choices=None,
+                                         nargs=None, action=None)
+        kwarg_dict['load_filename'] = Kwarg(default=None, description='Name of the load file',
+                                         parser=None, choices=None,
+                                         nargs=None, action=None)
         return kwarg_dict
 
     @classmethod
-    def apply(cls, stack, cyme_location, base_dir=None):
+    def apply(cls, stack, *args, **kwargs):
 
-        if not os.path.exists(os.path.join(base_dir,cyme_location)):
-            raise ValueError("No folder exists at {}".format(os.path.join(base_dir,cyme_location)))
+        if 'base_dir' in kwargs:
+            base_dir = kwargs['base_dir']
+        else:
+            base_dir = './'
+
+        #if not os.path.exists(os.path.join(base_dir,cyme_location)):
+        #    raise ValueError("No folder exists at {}".format(os.path.join(base_dir,cyme_location)))
+
+        if 'network_filename' in kwargs:
+            network_filename = kwargs['network_filename']
+        else:
+            network_filename = 'network.txt' #Default
+
+        if 'equipment_filename' in kwargs:
+            equipment_filename = kwargs['equipment_filename']
+        else:
+            equipment_filename = 'equipment.txt' #Default
+
+        if 'load_filename' in kwargs:
+            load_filename = kwargs['load_filename']
+        else:
+            load_filename = 'load.txt' #Default
 
         base_model = Store()
-        reader = CymeReader(data_folder_path=os.path.join(base_dir,cyme_location))
+        reader = CymeReader(data_folder_path=base_dir,
+                            network_filename=network_filename,
+                            equipment_filename=equipment_filename,
+                            load_filename=load_filename)
         reader.parse(base_model)
         base_model.set_names()
         stack.model = base_model
